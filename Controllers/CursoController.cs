430c62f1e6de43e9dd82582cf30014b66da92504
Lázaro Alves <geo.eolo@gmail.com>
2021-05-16 00:53:06 -0300

    Adicionando funcionalidades as telas

---
+++ Controllers/CursoController.cs
@@ -0,0 +1,24 @@
+﻿using curso.web.mvc.Models.Cursos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace curso.web.mvc.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(CadastrarCursoViewModelInput cadastrarCursoViewModelInput)
+        {
+            return View();
+        }
+
+        public IActionResult Listar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Cursos/CadastrarCursoViewModelInput.cs
@@ -0,0 +1,10 @@
+﻿namespace curso.web.mvc.Models.Cursos
+{
+    public class CadastrarCursoViewModelInput
+    { 
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
