2b10f9ad7fab62109c040860c6477168c1e39184
Lázaro Alves <geo.eolo@gmail.com>
2021-05-15 23:12:51 -0300

    Estrutura inicial telas

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using curso.web.mvc.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace curso.web.mvc.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(RegistrarUsuarioViewModelInput registrarUsuarioViewModelInput)
+        {
+            return View();
+        }
+
+        public IActionResult Logar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Usuarios/LoginViewModelInput.cs
@@ -0,0 +1,9 @@
+﻿namespace curso.web.mvc.Models.Usuarios
+{
+    public class LoginViewModelInput
+    {
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
